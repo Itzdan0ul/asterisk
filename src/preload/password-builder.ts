d687d0fba29148111acb5af4c6439797770a072a
Itzdan0ul <itzdan.ul@gmail.com>
2022-03-20 18:38:29 -0600

    Preload files

---
+++ src/preload/password-builder.ts
@@ -0,0 +1,18 @@
+import { ipcMain } from 'electron';
+import PasswordBuilder from '../main/password-builder'
+
+ipcMain.handle('main:password-builder', async (event: Electron.IpcMainInvokeEvent, args: any) => {
+  switch (args[0]) {
+    case 'GET_PASSWORD':
+      getPasswordHandler(event, args[1])
+      break
+    }
+});
+
+const getPasswordHandler = (event: Electron.IpcMainInvokeEvent, payload: any): void => {
+  const { checkboxGroup, count, radioGroup } = payload
+  const chars: string = PasswordBuilder.generate(radioGroup, checkboxGroup)
+  const password: string = PasswordBuilder.getPassword(chars, count)
+
+  event.sender.send('renderer:password-builder', password)
+}
