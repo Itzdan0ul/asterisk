eb1bbfb1695ee8a7760709032747bb2de0537804
Itzdan0ul <itzdan.ul@gmail.com>
2022-04-07 21:24:10 -0500

    Values were adjusted in relation to the changes made in the renderer, for the character tests

--- test/unit/main/platform.test.ts
+++ test/unit/main/platform.test.ts
@@ -4,13 +4,13 @@ import { Platform } from '../../../src/main/platform'
 describe('Platform', () => {
   it('Should be the same as the current platform.', () => {
     const platform: Platform = new Platform(os.platform())
-    expect(platform._platform).toEqual(os.platform())
+    expect(platform._name).toEqual(os.platform())
   })
   it('Should return the specified height according to the platform.', () => {
     const platform: Platform = new Platform(os.platform())
     const height: number = platform.setWindowHeight()
 
-    switch (platform._platform) {
+    switch (platform._name) {
       case 'win32':
         expect(height).toBe(364)
         break;
