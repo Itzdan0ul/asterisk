f07897e78a4c9d1e2104f7062041ac4396c324a6
Itzdan0ul <itzdan.ul@gmail.com>
2022-08-07 13:00:38 -0500

    refactor: Code refactoring and new channels added

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,25 +1,39 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electronAPI', {
-	invoke: (channel: string, data: any): void => {
-		const channels: string[] = ['main:password-builder'];
+  invoke: (channel: string, data: any): void => {
+    const channels: string[] = [
+      'main:password-builder',
+      'main:i18n',
+      'main:theme',
+      'main:help',
+      'main:window',
+    ];
 
-		if (channels.includes(channel)) {
-			ipcRenderer.invoke(channel, data);
-		}
-	},
-	once: (channel: string, func: Function): void => {
-		const channels: string[] = ['renderer:password-builder'];
-		
-		if (channels.includes(channel)) {
-			ipcRenderer.once(channel, (event: Electron.IpcRendererEvent, ...args: any) => func(...args));
-		}
-	},
-	on: (channel: string, func: Function): void => {
-		let channels: string[] = ['renderer:password-builder'];
-		
-		if (channels.includes(channel)) {
-			ipcRenderer.on(channel, (event: Electron.IpcRendererEvent, ...args: any) => func(...args));
-		}
-	}
+    if (channels.includes(channel)) {
+      ipcRenderer.invoke(channel, data);
+    }
+  },
+  once: (channel: string, func: Function): void => {
+    const channels: string[] = [
+      'renderer:password-builder:build-password',
+      'renderer:i18n:translate',
+      'renderer:i18n:get-locale',
+    ];
+
+    if (channels.includes(channel)) {
+      ipcRenderer.once(channel, (event: Electron.IpcRendererEvent, ...args: any) =>
+        func(...args)
+      );
+    }
+  },
+  on: (channel: string, func: Function): void => {
+    const channels: string[] = [];
+
+    if (channels.includes(channel)) {
+      ipcRenderer.on(channel, (event: Electron.IpcRendererEvent, ...args: any) =>
+        func(...args)
+      );
+    }
+  },
 });
