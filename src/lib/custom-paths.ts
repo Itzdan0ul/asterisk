cfef5b715deeaf9b07f415047efd467e57084232
Dan <69698529+Itzdan0ul@users.noreply.github.com>
2022-08-08 16:33:33 -0500

    Delete src/lib directory

--- src/lib/custom-paths.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import moduleAlias from 'module-alias';
-
-moduleAlias.addAliases({
-  '@src':  path.join(__dirname, '../'),
-  '@main': path.join(__dirname, '../main')
-});
